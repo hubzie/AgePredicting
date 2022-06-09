600a31ff2bb11316d7427c0a40c4e94b888d17fc
Hubert Zięba <hub.zieba@student.uj.edu.pl>
2022-06-09 23:24:49 +0200

    PCA demo

--- cpp/utils/PCA.cpp
+++ cpp/utils/PCA.cpp
@@ -17,7 +17,7 @@ PCA::PCA(const std::vector<Data>& data) {
     for(int i=0;i<n;i++)
         X.row(i) = data[i].x;
 
-    VectorXd mean = VectorXd::Zero(m);
+    mean = VectorXd::Zero(m);
     for(int i=0;i<n;i++) mean += X.row(i);
     mean /= n;
     for(int i=0;i<n;i++) X.row(i) -= mean;
@@ -28,22 +28,21 @@ PCA::PCA(const std::vector<Data>& data) {
 
     double frac = 0.0;
     auto s = svd.singularValues();
-    for(int i=0;i<s.size();i++) frac += s(i);
+    for(int i=0;i<s.size();i++) frac += s(i)*s(i);
     frac *= COMPRESSION;
 
     int l = 0;
     double sum = 0.0;
     for(int i=0;i<s.size();i++,l++) {
         if (sum > frac) break;
-        sum += s(i);
+        sum += s(i)*s(i);
     }
 
     cerr << "PCA: Compressed from " << m << " to " << l << " features" << endl;
 
-    this->V = svd.matrixV();
-    this->V.resize(l, m);
+    V = svd.matrixV();
+    V.conservativeResize(m, l);
 }
 
-VectorXd PCA::transform(const VectorXd& x) const {
-    return V * x;
-}
+VectorXd PCA::transform(const VectorXd& x) const { return V.transpose() * (x - mean); }
+MatrixXd PCA::getV() const { return V; }
